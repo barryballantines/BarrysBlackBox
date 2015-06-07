ed546b4d1f19fb679dd99b42de4d5d81fef4db21
barryballantines <barry.ballantines@mail.de>
2015-06-07 15:35:50 +0200

    KACARS: adding kacars-config to Services

--- src/main/java/ballantines/avionics/blackbox/Services.java
+++ src/main/java/ballantines/avionics/blackbox/Services.java
@@ -9,6 +9,8 @@ import ballantines.avionics.flightgear.connect.HttpPropertyServiceImpl;
 import ballantines.avionics.flightgear.connect.PropertyService;
 import ballantines.avionics.flightgear.connect.ServerConfig;
 import ballantines.avionics.blackbox.udp.FlightData;
+import ballantines.avionics.kacars.KAcarsConfig;
+import ballantines.avionics.kacars.KAcarsClient;
 import de.mbuse.pipes.Pipe;
 import de.mbuse.pipes.PipeUpdateListener;
 import java.util.Timer;
@@ -19,7 +21,7 @@ import java.util.prefs.Preferences;
  *
  * @author mbuse
  */
-public class Services implements PipeUpdateListener<Object>{
+public class Services implements PipeUpdateListener {
     
     // === STATICS ===
     
@@ -33,9 +35,10 @@ public class Services implements PipeUpdateListener<Object>{
     
     public void init() {
         ServerConfig serverConfig = getServerConfigFromUserPreferences();
+        KAcarsConfig kacarsConfig = readKACARSConfigFromUserPreferences();
         propertyService = new HttpPropertyServiceImpl(serverConfig); 
         flightDataRetrieval = new FGFlightDataRetrievalImpl(propertyService);
-        timer = new Timer("PIREP Recorder Timer");
+        timer = new Timer("Barry's BlackBox Timer");
         serverConfigPipe.set(serverConfig);
         udpServerPortPipe.set(5555);
         udpServerRunningPipe.set(false);
@@ -47,22 +50,51 @@ public class Services implements PipeUpdateListener<Object>{
     }
 
     @Override
-    public void pipeUpdated(Pipe<Object> pipe) {
+    public void pipeUpdated(Pipe pipe) {
         System.out.println("[SERVICES] Model updated : " + pipe.id() + " -> " + pipe.get());
         
-        if ("Services.serverConfig".equals(pipe.id())) {
+        if (pipe == this.serverConfigPipe) {
             ServerConfig config = (ServerConfig) pipe.get();
             propertyService.setServerConfig(config);
             writeServerConfigToUserPreferences(config);
-            
+        }
+        else if (pipe == this.kacarsConfigPipe) {
+            KAcarsConfig config = kacarsConfigPipe.get();
+            kacarsClient.setConfig(config);
+            writeKACARSConfigToUserPreferences(config);
+        }
+    }
+    
+    public KAcarsConfig readKACARSConfigFromUserPreferences() {
+        Preferences prefs = Preferences.userNodeForPackage(KAcarsConfig.class);
+        KAcarsConfig config = new KAcarsConfig();
+        config.url = prefs.get("config.url", null);
+        config.pilotID = prefs.get("config.user", null);
+        config.password = prefs.get("config.password", null);
+        config.enabled = prefs.getBoolean("config.enabled", false);
+        System.out.println("[SERVICES] reading kACARS config from user preferences: " + config);
+        return config;
+    }
+    
+    public void writeKACARSConfigToUserPreferences(KAcarsConfig config) {
+        try {
+            System.out.println("[SERVICES] writing kACARS config to user preferences: " + config);
+            Preferences prefs = Preferences.userNodeForPackage(KAcarsConfig.class);
+            prefs.put("config.url", config.url);
+            prefs.put("config.user", config.pilotID);
+            prefs.put("config.password", config.password);
+            prefs.putBoolean("config.enabled", config.enabled);
+            prefs.flush();
+        } catch (BackingStoreException ex) {
+            System.err.println("[SERVICES] Failed to write kACARS config to User Preferences: " + ex);
         }
     }
     
     public ServerConfig getServerConfigFromUserPreferences() {
-        Preferences prefs = Preferences.userRoot();
+        Preferences prefs = Preferences.userNodeForPackage(ServerConfig.class);
         
-        String host = prefs.get("de.mbuse.flightgear.pireprecorder.fgHost", "localhost");
-        int port = prefs.getInt("de.mbuse.flightgear.piperecorder.fgPort", 5500);
+        String host = prefs.get("flightgear.httpd.host", "localhost");
+        int port = prefs.getInt("flightgear.httpd.port", 5500);
         final ServerConfig serverConfig = new ServerConfig(host, port);
         
         System.out.println("[SERVICES] reading server config from user preferences: " + serverConfig);
@@ -75,9 +107,9 @@ public class Services implements PipeUpdateListener<Object>{
                 return;
             }
             System.out.println("[SERVICES] writing server config to user preferences: " + serverConfig);
-            Preferences prefs = Preferences.userRoot();
-            prefs.put("de.mbuse.flightgear.pireprecorder.fgHost", serverConfig.getHost());
-            prefs.putInt("de.mbuse.flightgear.pireprecorder.fgPort", serverConfig.getPort());
+            Preferences prefs = Preferences.userNodeForPackage(ServerConfig.class);
+            prefs.put("flightgear.httpd.host", serverConfig.getHost());
+            prefs.putInt("flightgear.httpd.port", serverConfig.getPort());
             prefs.flush();
         } catch (BackingStoreException ex) {
             System.err.println("[SERVICES] Failed to write ServerConfig to User Preferences: " + ex);
@@ -107,8 +139,10 @@ public class Services implements PipeUpdateListener<Object>{
     
     private HttpPropertyServiceImpl propertyService;
     private FlightDataRetrieval flightDataRetrieval;
+    private KAcarsClient kacarsClient;
     private Timer timer;
     
+    public final Pipe<KAcarsConfig> kacarsConfigPipe = Pipe.newInstance("Services.kacarsConfig", this);
     public final Pipe<ServerConfig> serverConfigPipe = Pipe.newInstance("Services.serverConfig", this);
     public final Pipe<Boolean> isRecordingPipe = Pipe.newInstance("Services.isRecording", this);
     public final Pipe<Long> currentFuelPipe = Pipe.newInstance("Services.currentFuel", this);
